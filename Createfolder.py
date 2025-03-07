7b5d2285570d1ba2dd6181fcd1701a22d0b8da7a
cv-beep <cvemsn11@gmail.com>
2025-03-07 11:17:56 +0300

    Update Createfolder.py

--- Createfolder.py
+++ Createfolder.py
@@ -19,8 +19,8 @@ def read_root(NAME:str,
     response = requests.post(str(f'{URLBITRIX}/disk.folder.addsubfolder.json'), json=inp_data)
     print(response)
     answ = json.loads(response.text)
-    deal_upd = {'id':DEALID,'fields':{'UF_CRM_DISK_URL':answ['DETAIL_URL']}}
+    deal_upd = {'id':DEALID,'fields':{'UF_CRM_DISK_URL':answ['result']['DETAIL_URL']}}
     response = requests.post(str(f'{URLBITRIX}/crm.deal.update'), json=deal_upd)
-    return {"data": answ['DETAIL_URL']}
+    return {"data": answ['result']['DETAIL_URL']}
 
 #disk.folder.addsubfolder.json?id=5271&data[NAME]=ТЕстов
