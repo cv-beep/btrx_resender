88c83d2c8744d3ae7d7057a903e8227a2a8db5c1
cv-beep <cvemsn11@gmail.com>
2025-03-14 14:27:09 +0300

    Update CreateLead.py

--- CreateLead.py
+++ CreateLead.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI
 import requests
 import json
+from pydantic import BaseModel
 app = FastAPI()
 URLBITRIX = os.environ['URLBITRIX']
 EMOJI = os.environ['EMOJI']
@@ -9,26 +10,47 @@ SOURCE_ID = os.environ['SOURCE_ID']
 WMIDFIELD = os.environ['WMIDFIELD'] #WMID USER FIELD
 TS_ID = os.environ['TS_ID'] #TRACKING_SOURCE_ID
 
+class Item(BaseModel):
+    NAME: str
+    PHONE: int
+    COMMENT: str
+    RECORD: str
+  
 @app.post("/addlead/")
-def read_root(NAME:str,
-              PHONE: int,
-              WMID: int| None = None,COMMENT: str| None = None,
-              UTM_SOURCE: str| None = None,UTM_MEDIUM:str| None = None,UTM_CAMPAIGN:str| None = None,UTM_CONTENT:str| None = None,UTM_TERM:str| None = None
-              ):
+def read_root(item: Item):
     lead_data = {'fields':{
             'TITLE':str(EMOJI + NAME),
-            'NAME': NAME,
+            'NAME': item.NAME,
             "STATUS_ID": "NEW",
-            "ASSIGNED_BY_ID ":"36",
             "SOURCE_ID": SOURCE_ID,
-            "TRACKING_SOURCE_ID": TS_ID,
-            "PHONE": [{ "VALUE": PHONE,"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
+            "PHONE": [{ "VALUE": Item.PHONE,"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
             'UTM_SOURCE': UTM_SOURCE,
             'UTM_MEDIUM':UTM_MEDIUM,
             'UTM_CAMPAIGN':UTM_CAMPAIGN,
             'UTM_CONTENT':UTM_CONTENT,
             'UTM_TERM':UTM_TERM,
-            'COMMENTS':COMMENT,
+            'COMMENTS': item.COMMENT,
+            WMIDFIELD: WMID  
+        }}
+    response = requests.post(str(f'{URLBITRIX}/crm.lead.add.json'), json=lead_data)
+    print(response)
+    answ = json.loads(response.text)
+    return {"data": answ['result']}
+
+@app.get("/addlead/")
+def read_root(item: Item):
+    lead_data = {'fields':{
+            'TITLE':str(EMOJI + NAME),
+            'NAME': item.NAME,
+            "STATUS_ID": "NEW",
+            "SOURCE_ID": SOURCE_ID,
+            "PHONE": [{ "VALUE": Item.PHONE,"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
+            'UTM_SOURCE': UTM_SOURCE,
+            'UTM_MEDIUM':UTM_MEDIUM,
+            'UTM_CAMPAIGN':UTM_CAMPAIGN,
+            'UTM_CONTENT':UTM_CONTENT,
+            'UTM_TERM':UTM_TERM,
+            'COMMENTS': item.COMMENT,
             WMIDFIELD: WMID  
         }}
     response = requests.post(str(f'{URLBITRIX}/crm.lead.add.json'), json=lead_data)
