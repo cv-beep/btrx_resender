d742614b25a334ad484bb74cdf599fa60bf638e7
cv-beep <cvemsn11@gmail.com>
2025-03-14 14:35:53 +0300

    Update CreateLead.py

--- CreateLead.py
+++ CreateLead.py
@@ -7,8 +7,6 @@ app = FastAPI()
 URLBITRIX = os.environ['URLBITRIX']
 EMOJI = os.environ['EMOJI']
 SOURCE_ID = os.environ['SOURCE_ID']
-WMIDFIELD = os.environ['WMIDFIELD'] #WMID USER FIELD
-TS_ID = os.environ['TS_ID'] #TRACKING_SOURCE_ID
 
 class Item(BaseModel):
     NAME: str
@@ -24,13 +22,7 @@ def read_root(item: Item):
             "STATUS_ID": "NEW",
             "SOURCE_ID": SOURCE_ID,
             "PHONE": [{ "VALUE": item.PHONE,"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
-            'UTM_SOURCE': UTM_SOURCE,
-            'UTM_MEDIUM':UTM_MEDIUM,
-            'UTM_CAMPAIGN':UTM_CAMPAIGN,
-            'UTM_CONTENT':UTM_CONTENT,
-            'UTM_TERM':UTM_TERM,
-            'COMMENTS': item.COMMENT,
-            WMIDFIELD: WMID  
+            'COMMENTS': item.COMMENT
         }}
     response = requests.post(str(f'{URLBITRIX}/crm.lead.add.json'), json=lead_data)
     print(response)
@@ -40,18 +32,12 @@ def read_root(item: Item):
 @app.get("/addlead/")
 def read_root(item: Item):
     lead_data = {'fields':{
-            'TITLE':str(EMOJI + NAME),
+            'TITLE':str(EMOJI + item.NAME),
             'NAME': item.NAME,
             "STATUS_ID": "NEW",
             "SOURCE_ID": SOURCE_ID,
-            "PHONE": [{ "VALUE": Item.PHONE,"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
-            'UTM_SOURCE': UTM_SOURCE,
-            'UTM_MEDIUM':UTM_MEDIUM,
-            'UTM_CAMPAIGN':UTM_CAMPAIGN,
-            'UTM_CONTENT':UTM_CONTENT,
-            'UTM_TERM':UTM_TERM,
-            'COMMENTS': item.COMMENT,
-            WMIDFIELD: WMID  
+            "PHONE": [{ "VALUE": item.PHONE,"VALUE_TYPE": "OTHER","TYPE_ID": "PHONE"}],
+            'COMMENTS': item.COMMENT
         }}
     response = requests.post(str(f'{URLBITRIX}/crm.lead.add.json'), json=lead_data)
     print(response)
