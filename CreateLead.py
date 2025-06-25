8123dd07bb36eed33e02578392e7ab5ef4c5961c
cv-beep <cvemsn11@gmail.com>
2025-06-25 12:43:55 +0300

    Update CreateLead.py
    not required

--- CreateLead.py
+++ CreateLead.py
@@ -11,7 +11,7 @@ TS_ID = os.environ['TS_ID'] #TRACKING_SOURCE_ID
 
 @app.post("/addlead/")
 def read_root(NAME:str,
-              TITLE:str,
+              TITLE:str| None = None,
               PHONE: int,
               WMID: int| None = None,COMMENT: str| None = None,
               UTM_SOURCE: str| None = None,UTM_MEDIUM:str| None = None,UTM_CAMPAIGN:str| None = None,UTM_CONTENT:str| None = None,UTM_TERM:str| None = None
